eabc1710cab1e219df65f5733bd0dc73149ec3f6
RivierGrullon <grullonrivier@gmail.com>
2024-08-27 17:08:23 -0400

    feat: Add new dependencies and translation module

---
+++ src/translate/index.ts
@@ -0,0 +1,45 @@
+import { I18n } from "i18n-js";
+import en from "./en";
+
+const i18n = new I18n({
+  en,
+});
+
+i18n.defaultLocale = "en";
+i18n.locale = "en";
+
+type Message = string | object;
+
+interface Interpolate {
+  [key: string]: any;
+}
+interface TranslateOptions {
+  message?: string | undefined;
+  defaults?: Array<{ message: Message } | undefined>;
+  defaultValue?: Message | undefined;
+  interpolate?: Interpolate;
+}
+declare module "i18n-js" {
+  interface CustomTypeOptions {
+    defaultNS: "en";
+    resources: {
+      en: typeof en;
+    };
+  }
+}
+
+type En = typeof en;
+type Leaves<T> = T extends Array<infer U>
+  ? `${number}.${Leaves<U>}` | `[${number}].${Leaves<U>}`
+  : T extends object
+  ? {
+      [K in keyof T]: `${Exclude<K, symbol>}${Leaves<T[K]> extends never
+        ? ""
+        : `.${Leaves<T[K]>}`}`;
+    }[keyof T]
+  : never;
+
+export const translate = (
+  scope: Leaves<En> | (string & {}),
+  options?: TranslateOptions
+) => i18n.t(scope, { ...options, ...options?.interpolate } as any);
